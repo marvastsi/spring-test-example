7e765e706eb55a6091b5ded7ce4beb7c028cb987
Marcelo Ferreira Vasconcelos <marcelo@accellog.com>
2020-04-09 12:07:22 -0300

    Updated tests to junit 5.

--- src/test/java/com/marvastsi/test_example/user/UserControllerTest.java
+++ src/test/java/com/marvastsi/test_example/user/UserControllerTest.java
@@ -11,42 +11,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.Arrays;
 
+import javax.transaction.Transactional;
+
+import com.marvastsi.test_example.TestApplication;
+
 //import org.apache.catalina.filters.CorsFilter;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
  
-@RunWith(SpringRunner.class)
-//@WebAppConfiguration
+@SpringBootTest(classes = { TestApplication.class,
+        UserController.class }, webEnvironment = WebEnvironment.DEFINED_PORT)
 @AutoConfigureMockMvc
-@SpringBootTest
 public class UserControllerTest {
  
 	@Autowired
     private MockMvc mockMvc;
  
-    @MockBean
+    @SpyBean
     private UserService userServiceMock;
     
     @InjectMocks
 	private UserController userController;
  
-    @Before
+    @BeforeEach
     public void setUp() {
-    	MockitoAnnotations.initMocks(this);
-		mockMvc = MockMvcBuilders.standaloneSetup(userController)
-				.build();
+//    	MockitoAnnotations.initMocks(this);
+//		mockMvc = MockMvcBuilders.standaloneSetup(userController)
+//				.build();
     }
  
     @Test
